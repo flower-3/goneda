d3ef598427d88f0294288bed37b04093b4007f30
youngun-park <youngun.park@navercorp.com>
2022-06-20 01:09:31 +0900

    feature: 셀레니움 설정 및 작동테스트
    - 설정파일 분리
    - 테스트코드 작성

--- goneda-core/src/main/java/com/gonedacore/Application.java
+++ goneda-core/src/main/java/com/gonedacore/Application.java
@@ -1,15 +1,11 @@
-package com.gonedaweb;
+package com.gonedacore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-
 @SpringBootApplication
-@EntityScan(basePackages = {"com.gonedacore", "com.gonedaweb"})
-@EnableJpaRepositories(basePackages = {"com.gonedacore", "com.gonedaweb"})
 public class Application {
 
     public static void main(String[] args) {
---
+++ goneda-core/src/test/java/com/gonedacore/utils/CustomerCrawlerTest.java
@@ -0,0 +1,30 @@
+package com.gonedacore.utils;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.gonedacore.config.SeleniumConfig;
+
+@SpringBootTest
+@ActiveProfiles("local")
+public class CustomerCrawlerTest {
+
+	@Autowired
+	ChromeDriver chromeDriver;
+
+	@Autowired
+	SeleniumConfig seleniumConfig;
+
+	@DisplayName("크롤러 정상동작 테스트")
+	@Test
+	public void workTest(){
+		chromeDriver.get("https://naver.com");
+		Assertions.assertThat("NAVER".equals(chromeDriver.getTitle()));
+	}
+
+}
