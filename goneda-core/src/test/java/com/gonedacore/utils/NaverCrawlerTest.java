04430cba65f747a8a1aa3db3e8478ad97b46fcfe
youngUnPark <mduddns@naver.com>
2022-07-23 14:19:56 +0900

    dd

--- goneda-core/src/test/java/com/gonedacore/utils/NaverCrawlerTest.java
+++ goneda-core/src/test/java/com/gonedacore/utils/NaverCrawlerTest.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.ActiveProfiles;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@ActiveProfiles({"local", "beta"})
+@ActiveProfiles({"local"})
 class NaverCrawlerTest {
 
     @Autowired
